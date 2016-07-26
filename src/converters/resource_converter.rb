85b3627caaa3cd80f64212732037fdb8296905e6
jambun <jambun@gmail.com>
2016-07-26 14:01:49 +1000

    use enclosure for derived dates and move to date class

--- src/converters/resource_converter.rb
+++ src/converters/resource_converter.rb
@@ -79,17 +79,7 @@ class ResourceConverter < Converter
         dates << bulk_date.merge({'date_type' => 'bulk', 'label' => 'creation'})
       end
 
-      # derived dates > date_type = inclusive, date_label = creation
-      # creation dates are derived. see:
-      # lib/CIDER/Schema/Result/ObjectWithDerivedFields
-      dates_query = "select min(i.item_date_from) as date_from, max(i.item_date_from) as date_from_to, " +
-        "max(i.item_date_to) as date_to " +
-        "from object o, item i where o.id = i.id and o.number like '#{number}%'"
-      result = db.fetch(dates_query).first
-      if result[:date_from]
-        dates << Dates.range(result[:date_from], (result[:date_to] || result[:date_from_to])).merge({'date_type' => 'inclusive',
-                                                                                                      'label' => 'creation'})
-      end
+      dates << Dates.enclosed_range(db, collection[:id])
 
       dates.compact!
 
